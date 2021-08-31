7870f60a3781922ec22bfa5a5756ab7047973843
TanLoi <loihaag12345@gmail.com>
2021-08-31 19:10:22 +0700

    change bcrypt to bcrypt-nodejs

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt-nodejs';
 
 export const updateUser = async (req,res) => {
     if(req.userId === req.params.id || req.body.isAdmin){
