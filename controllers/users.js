da4ae3be36c8e1e977cef6f6b16760b841a4c037
TanLoi <loihaag12345@gmail.com>
2021-09-08 12:40:57 +0700

    update user

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import Google from '../models/Google.js';
 
 export const updateUser = async (req,res) => {
     if(req.userId === req.params.id || req.body.isAdmin){
@@ -13,6 +14,29 @@ export const updateUser = async (req,res) => {
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    } else {
+        return res.status(403).json("You can update only your account!");
+    }
+};
+
+export const updateGoogle = async (req,res) => {
+    if(req.userId === req.params.id || req.body.isAdmin){
+        if(req.body?.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+        }
+        try {
+            const user = await Google.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             });
             res.status(200).json(user);
